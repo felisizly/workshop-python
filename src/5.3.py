a62d3fb647481f6a3972f6ef94515dd1f9fe3483
FelisiaMascarenhas205410143 <71498459+felisizly@users.noreply.github.com>
2023-03-30 21:35:13 +0700

    Add files via upload

---
+++ src/5.3.py
@@ -0,0 +1,14 @@
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
+'orange' in basket   
+'crabgrass' in basket
+a = set('abracadabra')
+b = set('alacazam')
+a  
+a-b
+a | b  
+a & b 
+a ^ b 
+
+a = {x for x in 'abracadabra' if x not in 'abc'}
+a
